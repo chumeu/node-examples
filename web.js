c6d646d38e7fb53aca409392b3c085dc2c2dbc59
unknown <chumeu@gmail.com>
2014-05-20 09:58:44 -0300

    alteracao web.js

--- web.js
+++ web.js
@@ -4,12 +4,14 @@ var logfmt = require("logfmt");
 var app = express();
 
 app.use(logfmt.requestLogger());
+var port = Number(process.env.PORT || 5000);
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
+  res.send("Listening on " + port);
+  console.log("Listening on " + port);
 });
 
-var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
